1b5996eefe4669486b03fc7f26d1bb78da33f701
HornedCoder <kayush5u2018@gmail.com>
2024-10-21 19:27:49 +0530

    Update fibo.py

--- DP/fibo.py
+++ DP/fibo.py
@@ -1,3 +1,5 @@
+#This below method is memoization which is a top down approach(meaning solving from top to bottom).
+
 def fibo(N, arr):
     if N == 1:
         return 0
@@ -13,4 +15,16 @@ def fibo(N, arr):
 myL = {}
 print(fibo(6, myL))
 print(myL)
+
+
+#The following is a bottom up approach called Tabulation.
+
+def fib2(N):
+    tb = [0,1]
+    for i in range(2, N+1):
+        tb.append(tb[i-1]+tb[i-2])
+    return tb[N-1]
+
+print(fib2(6))
+
      
